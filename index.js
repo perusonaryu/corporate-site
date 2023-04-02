36a792f938c5562642758f8c45cf1229654970dc
perusonaryu <perusonaryu1127@gmail.com>
2023-04-02 16:12:35 +0900

    💄 トップページ修正

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ $(function () {
   setTimeout(function () {
     $.scrollify({
       section: 'section',
-      scrollSpeed: 1000,
+      scrollSpeed: 1500,
       easing: 'swing',
       updateHash: false,
       setHeights: false,
-    });
-    $('#inianimation').addClass('d-n');
-    $('#white-back').addClass('d-n');
-    $('#green-back').addClass('d-n');
-  }, 6500);
-});
+    })
+    $('#inianimation').addClass('d-n')
+    $('#white-back').addClass('d-n')
+    $('#green-back').addClass('d-n')
+  }, 6500)
+
+  //各セクション遷移のためのクリックイベント定義
+  $('#top-list li').each(function (i, ele) {
+    $(this).click(function () {
+      $.scrollify.move(i + 1)
+    })
+
+    $('.section' + (i + 1)).click(function () {
+      $.scrollify.move(i)
+    })
+  })
+})
 
 $('.openbtn1').click(function () {
-  $(this).toggleClass('active');
-  $('#fadeout').toggleClass('animation');
-});
+  $(this).toggleClass('active')
+  $('#fadeout').toggleClass('animation')
+})
