e7e9ff810407824127e1cb03ad2263565bbf243d
perusonaryu <perusonaryu1127@gmail.com>
2023-06-03 21:54:30 +0900

    🐛 section1の時に矢印の要素が全面にでしまい一部クリックイベントが実行されないバグを修正

--- js/index.js
+++ js/index.js
@@ -66,9 +66,11 @@ $(window).on('load', function () {
         prevSecDesEle.addClass('close')
         prevDesBtnEle.removeClass('show')
         if (i == 0) {
+          $('#arrows').css('z-index','-1')
           $('#arrow-top').removeClass('active')
           $('#arrow-bottom').removeClass('active')
         } else {
+          $('#arrows').css('z-index','30')
           $('.slide-item' + i).slick('slickSetOption', 'autoplay', true, true)
           $('.slide-item' + prevSecIdx).slick('slickSetOption', 'autoplay', false, true)
           //各セクションの詳細テキスト、toggleボタン表示制御
